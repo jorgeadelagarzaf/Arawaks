eb1e0f59781bdb6be9b24e8c5267802a8f168c4f
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2024-12-01 18:35:06 -0600

    Update App.js
    Testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Arawakss TESTING 1234 WOW THIS WORKS
+          Edit <code>src/App.js</code> and save to reload. Arawakss TESTING 1234 WOW THIS WORKS TESTTTT12314234
         </p>
         <a
           className="App-link"
