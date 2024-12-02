bb2e80e89e3c092b0fc06ad6303f7ab127ec216d
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2024-12-01 18:27:50 -0600

    Update App.js
    Update Firebase test message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Arawakss
+          Edit <code>src/App.js</code> and save to reload. Arawakss TESTING 1234
         </p>
         <a
           className="App-link"
